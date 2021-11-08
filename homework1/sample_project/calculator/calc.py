4a3c2cae2991b16676fa051b012b54ff88e1e30d
Anvar000 <anvar19971403@gmail.com>
2021-11-08 16:37:18 +0300

    calculator

---
+++ homework1/sample_project/calculator/calc.py
@@ -0,0 +1,14 @@
+def check_power_of_2(a: int) -> bool:
+    """Function to check is the number 'a' a power of 2 or not
+
+    Args:
+        a (int): any number
+
+    Returns:
+        bool: True - if the number 'a' is a power of 2
+              False - if the number 'a' is not a power of 2
+    """
+    if a > 0:
+        return not (bool(a & (a - 1)))
+    else:
+        return False
